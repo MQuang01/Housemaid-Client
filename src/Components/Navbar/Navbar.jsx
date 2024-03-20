b3df6b25d8d3993352664ca8b0ff0e544204495d
NHAN\cnbnh <cnbnhan@gmail.com>
2024-03-20 11:31:01 +0700

    dich trang sang tieng viet

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
             <div className="container">
                 <div className="d-flex justify-content-between topbar py-2">
                     <div className="d-flex align-items-center flex-shrink-0 topbar-info">
-                        <a href="#" className="me-4 text-secondary"><i className="fas fa-map-marker-alt me-2 text-dark"></i>27 nguyen Tri Phuong</a>
+                        <a href="#" className="me-4 text-secondary"><i className="fas fa-map-marker-alt me-2 text-dark"></i>28 nguyen Tri Phuong</a>
                         <a href="#" className="me-4 text-secondary"><i className="fas fa-phone-alt me-2 text-dark"></i>+01234567890</a>
                         <a href="#" className="text-secondary"><i className="fas fa-envelope me-2 text-dark"></i>Example@gmail.com</a>
                     </div>
@@ -38,28 +38,28 @@ const Navbar = () => {
             <div className="container">
                 <nav className="navbar navbar-dark navbar-expand-lg py-lg-0">
                     <a href="index.html" className="navbar-brand">
-                        <h1 className="text-primary mb-0 display-5">Pest<span className="text-white">Kit</span><i className="fa fa-spider text-primary ms-2"></i></h1>
+                        <h1 className="text-primary mb-0 display-5">House<span className="text-white">Maid</span><i className="fa fa-broom text-primary ms-2"></i></h1>
                     </a>
                     <button className="navbar-toggler bg-primary" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                         <span className="fa fa-bars text-dark"></span>
                     </button>
                     <div className="collapse navbar-collapse me-n3" id="navbarCollapse">
                         <div className="navbar-nav ms-auto">
-                            <a href="index.html" className="nav-item nav-link active">Home</a>
-                            <a href="about.html" className="nav-item nav-link">About</a>
-                            <a href="service.html" className="nav-item nav-link">Services</a>
-                            <a href="project.html" className="nav-item nav-link">Projects</a>
+                            <a href="index.html" className="nav-item nav-link active">Trang chủ</a>
+                            <a href="about.html" className="nav-item nav-link">Thông tin</a>
+                            <a href="service.html" className="nav-item nav-link">Dịch vụ</a>
+                            {/*<a href="project.html" className="nav-item nav-link">Dự án</a>*/}
                             <div className="nav-item dropdown">
-                                <a href="#" className="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                                <a href="#" className="nav-link dropdown-toggle" data-bs-toggle="dropdown">Mục lục</a>
                                 <div className="dropdown-menu m-0 bg-primary">
-                                    <a href="price.html" className="dropdown-item">Pricing Plan</a>
-                                    <a href="blog.html" className="dropdown-item">Blog Post</a>
-                                    <a href="team.html" className="dropdown-item">Team Members</a>
+                                    <a href="price.html" className="dropdown-item">Bảng giá</a>
+                                    {/*<a href="blog.html" className="dropdown-item">Blog Post</a>*/}
+                                    {/*<a href="team.html" className="dropdown-item">Team Members</a>*/}
                                     <a href="testimonial.html" className="dropdown-item">Testimonial</a>
-                                    <a href="404.html" className="dropdown-item">404 Page</a>
+                                    {/*<a href="404.html" className="dropdown-item">404 Page</a>*/}
                                 </div>
                             </div>
-                            <a href="contact.html" className="nav-item nav-link">Contact</a>
+                            <a href="contact.html" className="nav-item nav-link">Liên hệ</a>
                         </div>
                     </div>
                 </nav>
