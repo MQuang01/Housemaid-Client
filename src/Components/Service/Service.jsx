d4bda43f7dac02a26bfef8699a96a6afd2c47f72
NHAN\cnbnh <cnbnhan@gmail.com>
2024-03-20 10:07:00 +0700

    nhan chinh sua service

--- src/Components/Service/Service.jsx
+++ src/Components/Service/Service.jsx
@@ -13,21 +13,23 @@ const Service = () => {
         // marginBottom: '20px',
         borderRadius: '22px',
         alignItems: 'center', justifyContent: 'center', display: 'flex'
-
-}
+    }
+    const h4size ={
+        height: '40px'
+    }
     return(
         <div className="container-fluid services py-5">
             <div className="container text-center py-5">
                 <div className="text-center mb-5 wow fadeInUp" data-wow-delay=".3s">
-                    <h5 className="mb-2 px-3 py-1 text-dark rounded-pill d-inline-block border border-2 border-primary">Our Services</h5>
-                    <h1 className="display-5">Our Featured Services</h1>
+                    <h5 className="mb-2 px-3 py-1 text-dark rounded-pill d-inline-block border border-2 border-primary">Our Featured Services</h5>
+                    {/*<h1 className="display-5">Our  Services</h1>*/}
                     <h2>Hire professionals, Experienced specifically for your needs</h2>
                 </div>
 
                 <div className="row g-5" style={{'--bs-gutter-x': '0.1rem' }}>
 
 
-                    <div className="col-xxl-2 col-lg-4 col-md-6 col-sm-12 mx-auto wow fadeInUp" data-wow-delay=".3s">
+                    <div className="col-xxl-2 col-lg-4 col-md-6 col-sm-12 mx-auto wow fadeInUp service-tag" data-wow-delay=".3s">
                         <div className="bg-light rounded p-5 services-item">
                             <div className="d-flex" style={{ alignItems: 'center', justifyContent: 'center', display: 'flex' }}>
                                 <div className="mb-4">
@@ -36,51 +38,51 @@ const Service = () => {
                                 </div>
                             </div>
                             {/*<img src={domestichelp} style={imgService} alt="domestichelp"/>*/}
-                            <h4>Domestic Help</h4>
+                            <h4 style={h4size}>Domestic Help</h4>
 
                         </div>
                     </div>
-                    <div className="col-xxl-2 col-lg-4 col-md-6 col-sm-12 mx-auto wow fadeInUp" data-wow-delay=".5s">
+                    <div className="col-xxl-2 col-lg-4 col-md-6 col-sm-12 mx-auto wow fadeInUp service-tag" data-wow-delay=".5s">
                         <div className="bg-light rounded p-5 services-item">
                             <div className="d-flex" style={{ alignItems: 'center', justifyContent: 'center', display: 'flex' }}>
                                 <div className="mb-4">
                                     <img src={cook} style={imgService} alt="domestichelp"/>
                                 </div>
                             </div>
-                            <h4 className="text-center">Cooks</h4>
+                            <h4 className="text-center" style={h4size}>Cooks</h4>
 
                         </div>
                     </div>
-                    <div className="col-xxl-2 col-lg-4 col-md-6 col-sm-12 mx-auto wow fadeInUp" data-wow-delay=".7s">
+                    <div className="col-xxl-2 col-lg-4 col-md-6 col-sm-12 mx-auto wow fadeInUp service-tag" data-wow-delay=".7s">
                         <div className="bg-light rounded p-5 services-item">
                             <div className="d-flex" style={{ alignItems: 'center', justifyContent: 'center', display: 'flex' }}>
                                 <div className="mb-4">
                                     <img src={babysitter} style={imgService} alt="domestichelp"/>
                                 </div>
                             </div>
-                            <h4 className="text-center">Babysitter</h4>
+                            <h4 className="text-center" style={h4size}>Babysitter</h4>
 
                         </div>
                     </div>
-                    <div className="col-xxl-2 col-lg-4 col-md-6 col-sm-12 mx-auto wow fadeInUp" data-wow-delay=".9s">
+                    <div className="col-xxl-2 col-lg-4 col-md-6 col-sm-12 mx-auto wow fadeInUp service-tag" data-wow-delay=".9s">
                         <div className="bg-light rounded p-5 services-item">
                             <div className="d-flex" style={{ alignItems: 'center', justifyContent: 'center', display: 'flex' }}>
                                 <div className="mb-4">
                                     <img src={allrounder} style={imgService} alt="domestichelp"/>
                                 </div>
                             </div>
-                            <h4 className="text-center">All-rounders</h4>
+                            <h4 className="text-center" style={h4size}>All-rounders</h4>
 
                         </div>
                     </div>
-                    <div className="col-xxl-2 col-lg-4 col-md-6 col-sm-12 mx-auto wow fadeInUp" data-wow-delay=".9s">
+                    <div className="col-xxl-2 col-lg-4 col-md-6 col-sm-12 mx-auto wow fadeInUp service-tag" data-wow-delay=".9s">
                         <div className="bg-light rounded p-5 services-item">
                             <div className="d-flex" style={{ alignItems: 'center', justifyContent: 'center', display: 'flex' }}>
                                 <div className="mb-4">
                                     <img src={livein} style={imgService} alt="domestichelp"/>
                                 </div>
                             </div>
-                            <h4 className="text-center">24 Hrs - Live In</h4>
+                            <h4 className="text-center" style={h4size}>24 Hrs - Live In</h4>
 
                         </div>
                     </div>
