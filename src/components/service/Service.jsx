39ffb87c1ffdf2cd8cfc778bdff397f34e5acee2
Quang <hcmquang2111@gmail.com>
2024-03-22 08:50:16 +0700

    update clean code

--- src/components/service/Service.jsx
+++ src/components/service/Service.jsx
@@ -37,75 +37,7 @@ const Service = () => {
                     <h2>Với trình độ nghiệp vụ được đào tạo bài bản, nhân viên tại đây luôn làm việc với tinh thần trách
                         nhiệm cao nhất.</h2>
                 </div>
-
-<<<<<<< HEAD:src/components/service/Service.jsx
-                <div className="row g-5" style={{'--bs-gutter-x': '0.1rem' }}>
-
-
-                    <div className="col-xxl-2 col-lg-4 col-md-6 col-sm-12 mx-auto wow fadeInUp service-tag" data-wow-delay=".3s">
-                        <Link to="/booking" onClick={() => window.scrollTo(0, 0)}>
-                        <div className="bg-light rounded p-5 services-item">
-                            <div className="d-flex" style={{ alignItems: 'center', justifyContent: 'center', display: 'flex' }}>
-                                <div className="mb-4">
-                                    <img src={"/assets/img/domestichelp.jpg"} style={imgService} alt="domestichelp"/>
-                                </div>
-                            </div>
-                            {/*<img src={domestichelp} style={imgService} alt="domestichelp"/>*/}
-                            <h4 style={h4size}>Giúp việc nhà</h4>
-
-                            <RatingStars rating={3.4}/>
-                        </div>
-                        </Link>
-                    </div>
-                    <div className="col-xxl-2 col-lg-4 col-md-6 col-sm-12 mx-auto wow fadeInUp service-tag" data-wow-delay=".5s">
-                        <div className="bg-light rounded p-5 services-item">
-                            <div className="d-flex" style={{ alignItems: 'center', justifyContent: 'center', display: 'flex' }}>
-                                <div className="mb-4">
-                                    <img src={"assets/img/cook.jpg"} style={imgService} alt="domestichelp"/>
-                                </div>
-                            </div>
-                            <h4 className="text-center" style={h4size}>Nấu ăn</h4>
-                            <RatingStars rating={3.4}/>
-
-                        </div>
-                    </div>
-                    <div className="col-xxl-2 col-lg-4 col-md-6 col-sm-12 mx-auto wow fadeInUp service-tag" data-wow-delay=".7s">
-                        <div className="bg-light rounded p-5 services-item">
-                            <div className="d-flex" style={{ alignItems: 'center', justifyContent: 'center', display: 'flex' }}>
-                                <div className="mb-4">
-                                    <img src={"/assets/img/babysitter.jpg"} style={imgService} alt="domestichelp"/>
-                                </div>
-                            </div>
-                            <h4 className="text-center" style={h4size}>Trông trẻ</h4>
-                            <RatingStars rating={3.4}/>
-
-                        </div>
-                    </div>
-                    <div className="col-xxl-2 col-lg-4 col-md-6 col-sm-12 mx-auto wow fadeInUp service-tag" data-wow-delay=".9s">
-                        <div className="bg-light rounded p-5 services-item">
-                            <div className="d-flex" style={{ alignItems: 'center', justifyContent: 'center', display: 'flex' }}>
-                                <div className="mb-4">
-                                    <img src={"/assets/img/allrounder.png"} style={imgService} alt="domestichelp"/>
-                                </div>
-                            </div>
-                            <h4 className="text-center" style={h4size}>Giúp việc đa năng</h4>
-                            <RatingStars rating={3.4}/>
-
-                        </div>
-                    </div>
-                    <div className="col-xxl-2 col-lg-4 col-md-6 col-sm-12 mx-auto wow fadeInUp service-tag" data-wow-delay=".9s">
-                        <div className="bg-light rounded p-5 services-item">
-                            <div className="d-flex" style={{ alignItems: 'center', justifyContent: 'center', display: 'flex' }}>
-                                <div className="mb-4">
-                                    <img src={"/assets/img/livein.jpeg"} style={imgService} alt="domestichelp"/>
-                                </div>
-                            </div>
-                            <h4 className="text-center" style={h4size}>Giúp việc 24/7</h4>
-                            <RatingStars rating={3.4}/>
-
-                        </div>
-                    </div>
-=======
+                
                 <div className="row g-5" style={{'--bs-gutter-x': '0.1rem'}}>
                     {categories.map((category, index) => {
                         return (
@@ -128,7 +60,6 @@ const Service = () => {
                         )
                     })}
 
->>>>>>> 7e4c943eaa07c987a191d01b02a8b04f500a5e96:src/Components/Service/Service.jsx
                 </div>
 
             </div>
