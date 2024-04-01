823239cac770d0f6afa72b350a1c8c2245c32802
Quang <hcmquang2111@gmail.com>
2024-04-01 09:06:19 +0700

    fix bug order

--- src/until/InforUrl.js
+++ src/until/InforUrl.js
@@ -1,2 +1,2 @@
-// export const InforUrl = "http://192.168.1.36:8080/api";
-export const InforUrl = "http://localhost:8080/api";
+export const InforUrl = "http://192.168.1.34:8080/api";
+// export const InforUrl = "http://localhost:8080/api";
